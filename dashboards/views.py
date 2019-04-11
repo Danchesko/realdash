b36672d971ee944b5df8fef22e891c8477c26540
Dan Berdikulov <danberdov@gmail.com>
2019-04-11 19:18:09 +0600

    update views

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from pytz import UTC
+
+from .models import DashboardData
+
+
+def dashboard_current_time(request):
+    now = datetime.now()
+    timestamp = datetime(2019, 1, 1, 
+                         now.hour, now.minute, now.second, 0, UTC)
+    return _dashboard_render(request, timestamp)
+
+
+def dashboard_request_time(request, hour, minute, second):
+    try:
+        timestamp = datetime(2019, 1, 1,
+                             int(hour), int(minute),
+                             int(second), 0, UTC)
+    except ValueError:
+        raise Http404('Invalid time requested')
+    return _dashboard_render(request, timestamp)
+
+
+def _dashboard_render(request, timestamp):
+    try:
+        data = DashboardData.objects.get(timestamp=timestamp)
+    except DashboardData.DoesNotExist:
+        raise Http404('Could not find {}'.format(timestamp))
+    return render(request, 'index.html', {'data': data})

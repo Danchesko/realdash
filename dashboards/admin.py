c50c56653444de447c10d4e7ce089d83797c7c18
Dan Berdikulov <danberdov@gmail.com>
2019-04-11 18:50:53 +0600

    add admin

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DashboardData
+
+@admin.register(DashboardData)
+class DashboardAdmin(admin.ModelAdmin):
+    list_display = ['servername', 'timestamp', 'memory_load', 'cpu_load']

5bbf5777f0e23ca9a50823f8d43dc589aa149cff
Dan Berdikulov <danberdov@gmail.com>
2019-04-12 18:36:52 +0600

    update urls

--- realdash/urls.py
+++ realdash/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 from dashboards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', views.documentation),
-    re_path(r'^$', views.dashboard_current_time),
-    re_path(r'^t=(\d{2}):(\d{2}):(\d{2})$', views.dashboard_request_time),
+    path('admin/', admin.site.urls, name='admin'),
+    path('docs/', views.documentation, name='docs'),
+    re_path(r'^$', views.dashboard_current_time, name='index'),
+    re_path(r'^t=(\d{2}):(\d{2}):(\d{2})$', views.dashboard_request_time, name='index_with_arg'),
 ]
